790aa607644565d8e3c63ff18e656b59405accbb
Kamenskih Dmitriy <coolstreetassassin@gmail.com>
2024-01-01 17:52:08 +0400

    fix reading config

--- src/setConfig.ts
+++ src/setConfig.ts
@@ -14,10 +14,10 @@ type Properties = {
 
 export const setConfig = async ({ root }: Properties): Promise<Config> => {
     // @ts-ignore
-    let res: Config = await import('../defaultConfig.js');
+    let res: Config = (await import('../defaultConfig.js')).default;
     const localConfigPath = path.resolve(root, CONFIG_FILE_NAME);
     if (fs.existsSync(localConfigPath)) {
-        const manualConfig: Config = await import(localConfigPath);
+        const manualConfig: Config = (await import(localConfigPath)).default;
         res = { ...res, ...manualConfig, placeholders: { ...res.placeholders, ...manualConfig.placeholders } };
         if (Array.isArray(res.folderPath)) {
             res.folderPath = res.folderPath.map((p) => prepareFolderPath(p));
